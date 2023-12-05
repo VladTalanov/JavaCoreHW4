e1c24aa225a6f0398d7a42f63b8dd2e4867421b4
Влад Таланов <111172960+VladTalanov@users.noreply.github.com>
2023-12-05 22:26:44 +0300

    Create InsufficientFundsException.java

---
+++ InsufficientFundsException.java
@@ -0,0 +1,26 @@
+public class InsufficientFundsException extends RuntimeException{
+
+    private double balance;
+
+    private double requestedAmount;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getRequestedAmount() {
+        return requestedAmount;
+    }
+
+    /**
+     * Класс исключений выбрасываемый при запросе денежных средст сверх доступной величины
+     * @param message выводимое соодщение
+     * @param balance текущий баланс
+     * @param requestedAmount запрашиваемые средства
+     */
+    public InsufficientFundsException(String message, double balance, double requestedAmount){
+        super(message);
+        this.balance = balance;
+        this.requestedAmount = requestedAmount;
+    }
+}
