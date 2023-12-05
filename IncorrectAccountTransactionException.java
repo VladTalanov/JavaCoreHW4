f67ffca231bb9f4a0447cfb7630eb8d3008c48d7
Влад Таланов <111172960+VladTalanov@users.noreply.github.com>
2023-12-05 22:27:32 +0300

    Create IncorrectAccountTransactionException.java

---
+++ IncorrectAccountTransactionException.java
@@ -0,0 +1,12 @@
+public class IncorrectAccountTransactionException extends IllegalArgumentException {
+    private int accountNumber;
+
+    public IncorrectAccountTransactionException(String message, int accountNumber) {
+        super(message);
+        this.accountNumber = accountNumber;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+}
