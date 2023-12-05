edb3d4d5bfda61bfed4e4a200d9bde372e03e0c7
Влад Таланов <111172960+VladTalanov@users.noreply.github.com>
2023-12-05 22:27:15 +0300

    Create IncorrectAmountTransactionException.java

---
+++ IncorrectAmountTransactionException.java
@@ -0,0 +1,11 @@
+public class IncorrectAmountTransactionException extends IllegalArgumentException {
+    private double amount;
+    public IncorrectAmountTransactionException(String message, double amount) {
+        super(message);
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
